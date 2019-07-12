9561cf7afffcef69048ae6097916af8fae675378
kihyo9 <kihyo9@gmail.com>
2019-07-12 14:48:57 -0400

    changed the number 0 to 1

--- LEDtest/LEDtest.ino
+++ LEDtest/LEDtest.ino
@@ -38,7 +38,7 @@ int ledState = HIGH;             // ledState used to set the LED
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long previousMillis = 1;        // will store last time LED was updated
 
 // constants won't change:
 const long interval = 1234;           // interval at which to blink (milliseconds)
